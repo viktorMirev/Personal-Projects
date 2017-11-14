23197ea2f5eff6133b053d401657bd8691025de6
Viktor Mirev <viktor.mirev@gmail.com>
2017-11-14 19:50:34 +0200

    Financial Help
    This program helps me and my roommate to organise our shared expenses. The data is saved in .txt file.

---
+++ MoneyOrganisationProgram/MoneyBalance/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+
+
+namespace MoneyBalance
+{
+    public partial class Form1 : Form
+    {
+        decimal mirevBalance = 0m;
+        decimal kosyoBalance = 0m;
+        decimal difference = 0m;
+        decimal mustPay = 0m;
+        decimal mirevCurrDotation = 0m;
+        decimal kosyoCurrDotation = 0m;
+
+        public Form1()
+        {
+            InitializeComponent();
+            decimal[] balanceData = (File.ReadAllLines("data.txt")).ToList().Select(decimal.Parse).ToArray();
+            mirevBalance = balanceData[0];
+            kosyoBalance = balanceData[1];
+            RefreshIt();
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            if(decimal.TryParse(MirevBox.Text, out mirevCurrDotation))
+            {
+                this.mirevBalance += mirevCurrDotation;
+            }
+            if(decimal.TryParse(KosyoBox.Text, out kosyoCurrDotation))
+            {
+                this.kosyoBalance += kosyoCurrDotation;
+            }
+            string[] newBalance = new string[2] { mirevBalance.ToString(), kosyoBalance.ToString() };
+            File.WriteAllLines("data.txt", newBalance);
+            MirevBox.Text = null;
+            KosyoBox.Text = null;
+            RefreshIt();
+        }
+
+        private void RefreshIt()
+        {
+            difference = mirevBalance - kosyoBalance;
+            DifferenceLabel.Text = Math.Abs(difference).ToString();
+            if (difference != 0) ColorSwitch();
+            else
+            {
+                MirevLabel.ForeColor = Color.WhiteSmoke;
+                KoseLabel.ForeColor = Color.WhiteSmoke;
+                MustPayLabel.Text = "0";
+            }
+
+            AllMoneyLabel.Text = (mirevBalance + kosyoBalance).ToString();
+           
+        }
+
+        private void ColorSwitch()
+        {
+            mustPay = difference / 2;
+            if ( difference < 0)
+            {
+                MirevLabel.ForeColor = Color.Red;
+                KoseLabel.ForeColor = Color.WhiteSmoke;
+            }
+            else
+            {
+                MirevLabel.ForeColor = Color.WhiteSmoke;
+                KoseLabel.ForeColor = Color.Red;
+            }
+            MustPayLabel.Text = Math.Abs(mustPay).ToString();
+        }
+
+        private void MustPayLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MirevBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r') Calculate_Click(sender,e);
+        }
+
+        private void KosyoBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r') Calculate_Click(sender, e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
