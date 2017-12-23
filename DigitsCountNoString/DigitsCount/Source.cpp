4d8db2bb5453cd6df1e2403e6ce569c4f8769cf6
Viktor Mirev <viktor.mirev@gmail.com>
2017-12-24 01:28:05 +0200

    comments
    different arrangement

--- DigitsCountNoString/DigitsCount/Source.cpp
+++ DigitsCountNoString/DigitsCount/Source.cpp
@@ -1,3 +1,19 @@
+/**
+*
+* Solution to second homework task
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2017/2018
+*
+* @author Viktor Mirev
+* @idnumber 62231
+* @task 3
+* @compiler VC
+*
+*/
+
+
+
 #include<iostream>
 
 using namespace std;
@@ -5,6 +21,46 @@ using namespace std;
 char const MAX_INT_AS_STRING[11] = { "2147483647" };
 char const MIN_INT_AS_STRING[11] = { "2147483648" };
 int const INT_MAX_NUMBER_OF_DIGITS = 10;
+
+//functions
+bool IsGreaterThan(char first[100], char const second[100]);
+int InputHandle(char number[100]);
+int DigitHandle(char number[100], int length, int digits[10]);
+
+
+
+int main()
+{
+	char number[100];
+	int digits[10];
+
+	//default 0
+	for (int i = 0; i < 10; i++)
+	{
+		digits[i] = 0;
+	}
+
+	int length = InputHandle(number);
+	//input varification
+	if (length == -1)
+	{
+		cout << "-1" << endl;
+		//	system("pause");
+		return 0;
+	}
+
+
+	int maxNumber = DigitHandle(number, length, digits);
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (digits[i] == maxNumber) cout << i << "-->" << maxNumber << endl;
+	}
+
+	//system("pause");
+	return 0;
+
+}
 
 bool IsGreaterThan(char first[100], char const second[100])
 {
@@ -71,38 +127,4 @@ int DigitHandle(char number[100],int length, int digits[10])
 		if (max < digits[index]) max = digits[index];
 	}
 	return max;
-}
-
-
-int main()
-{
-	char number[100];
-	int digits[10];
-
-	//default 0
-	for (int i = 0; i < 10; i++)
-	{
-		digits[i] = 0;
-	}
-
-	int length = InputHandle(number);
-	//input varification
-	if (length == -1)
-	{
-		cout << "-1" << endl;
-		system("pause");
-		return 0;
-	}
-
-
-	int maxNumber = DigitHandle(number,length,digits);
-
-	for (int i = 0; i < 10; i++)
-	{
-		if (digits[i] == maxNumber) cout <<i<< "-->" << maxNumber << endl;
-	}
-
-	system("pause");
-	return 0;
-
 }
