3f2f6d0a2d54a13dbe93cf111f1c8fedce343990
Viktor Mirev <viktor.mirev@gmail.com>
2018-01-13 13:58:44 +0200

    Comments and some beauty changes

--- BracketChecker/BracketChecker/Source.cpp
+++ BracketChecker/BracketChecker/Source.cpp
@@ -10,10 +10,17 @@ bool EqualityBrecketChecker(char * indx)
 
 	if (*indx == '(') 	bracketCounter++;
 
-	if (*indx == ')') { bracketCounter--;		if (bracketCounter < 0) return false; }
+	if (*indx == ')') 
+	{
+		bracketCounter--;
+		//if the counter is less than 0
+		//there was ')' and no '(' to open it
+		if (bracketCounter < 0) return false; 
+	}
 	
 	indx++;
 
+	//return the function for the next
 	return EqualityBrecketChecker(indx);
 }
 
@@ -22,11 +29,13 @@ int main()
 	char * arr = new char[100];
 	cin.getline(arr, 100);
 	
+	//if the bracketCounter is !=0 we have different number '(' and ')'
 	if (EqualityBrecketChecker(arr) && bracketCounter == 0)
 		cout << "correct" << endl;
 	else
 	{
 		cout << "incorrect" << endl;
 	}
-	return main();
+
+	return 0;
 }
