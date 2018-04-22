97dda88acd369935fc28572e8a87bcf57b47166c
Viktor Mirev <viktor.mirev@gmail.com>
2018-04-22 18:35:49 +0300

    Product .h & .cpp
    Task : 
    Задача 3. Онлайн магазин								4 точки
    
    Да се моделира онлайн магазин (Store) за определени продукти (тениски, ризи, маратонки, GSM, лаптопи, автомобилни гуми, и др.). За целта:
    •	Изберете продукт, за който ще реализирате магазина и определете характеристиките на продукта (например марка, модел, категория, цвят, размер, операционна система, памет, и др.)
    •	Да се дефинира клас Product, който има следните член-данни:
    o	SKU (stock keeping unit) – уникален номер на продукта (задължително);
    o	Brand – марка (задължително)
    o	Model – модел (задължително)
    o	Category – категория (ако е приложимо)
    o	Price – единична цена (задължително)
    o	Count – брой налични продукта в магазина (задължително)
    o	Други член данни в зависимост от избрания продукт и определените характеристики.
    o	Използвайте коментари за класа и член-данните, за да е ясно какъв е избраният продукт и какви са неговите характеристики.
    •	Да се дефинират съответни методи за създаване, четене, запис и промяна на продукти;
    •	Да се дефинира клас Store, който съдържа динамичен списък (масив) от продукти в магазина, заедно с техния брой. Да се дефинират съответни методи за:
    o	Добавяне на нов продукт;
    o	Изтриване на продукт;
    o	Промяна на продукт;
    o	Извеждане на екрана на списък с наличните продукти и техния брой;
    o	Записване на всички продукти във файл;
    o	Зареждане на продуктите от файл.
    •	Да се дефинира главна програма, реализираща следното меню:
    A	Add new product
    X	Delete product
    C	Change product
    D	Display products
    S	Save products
    L	Load products
    Q	Quit

---
+++ OnlineStore/OnlineStore/Product.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include"Product.h"
+
+int Product::skuProvider = 0;
+
+Product::Product(char * brand, char * model, char * size, char * color, double price, int count)
+{
+	//it is static so it provides different id s all the time
+	this->SKU = this->skuProvider++;
+
+	this->brand = brand;
+	this->model = model;
+	this->size = size;
+	this->color = color;
+	this->price = price;
+	this->count = count;
+}
+
+Product::Product(const Product & other)
+{
+	this->CopyFromOther(other);
+}
+
+Product::~Product()
+{
+	this->Delete();
+}
+
+void Product::operator=(const Product & other)
+{
+	this->Delete();
+	CopyFromOther(other);
+}
+
+void Product::Print()
+{
+	std::cout << "FULL PRODUCT DESCRIPTION:" << std::endl;
+	std::cout << "brand: " << this->brand<< std::endl;
+	std::cout << "model: " << this->model << std::endl;
+	std::cout << "size: " << this->size << std::endl;
+	std::cout << "color: " << this->color << std::endl;
+	std::cout << "price: " << this->price << std::endl;
+	std::cout << "count: " << this->count << std::endl;
+}
+
+void Product::SetPrice(double price)
+{
+	this->price = price;
+}
+
+void Product::SetCount(int count)
+{
+	this->count = count;
+}
+
+void Product::Delete()
+{
+	delete[] this->brand;
+
+	delete[] this->model;
+
+	delete[] this->size;
+
+	delete[] this->color;
+}
+
+void Product::CopyFromOther(const Product& other)
+{
+	this->SKU = this->skuProvider++;
+
+	int len = strlen(other.brand) + 1;
+	this->brand = new char[len];
+	memcpy(this->brand, other.brand, len);
+
+	len = strlen(other.model) + 1;
+	this->model = new char[len];
+	memcpy(this->model, other.model, len);
+
+	len = strlen(other.size) + 1;
+	this->size = new char[len];
+	memcpy(this->size, other.size, len);
+
+	len = strlen(other.color) + 1;
+	this->color = new char[len];
+	memcpy(this->color, other.color, len);
+
+	this->price = price;
+	this->count = count;
+}
---
+++ OnlineStore/OnlineStore/Product.h
@@ -0,0 +1,66 @@
+#pragma once
+
+class Product
+{
+public:
+	//Constructor
+	Product(char* brand, char * model, char * size, char * color, double price, int count);
+
+	//Copy Constructor
+	Product(const Product& other);
+
+	//Destructor
+	~Product();
+
+	//Operator =
+	void operator=(const Product& other);
+
+	//Printing function
+	void Print();
+
+	//Mutators
+
+
+	//Only the price and the count can be changed 
+	//It is not possible to alter the size or the color of a T-shirt
+	//If you have to change it , it is another model so you have to delete 
+	//the current one and create new
+
+	//Price change
+	void SetPrice(double price);
+
+	//Count change
+	void SetCount(int count);
+
+private:
+	static int skuProvider;
+
+	//deletes all the dynamic data
+	void Delete();
+
+
+	//copies from other product
+	void CopyFromOther(const Product& other);
+
+	//Unique product number
+	int SKU;
+
+	//Brand of the T-shirt
+	char * brand;
+
+	//The name of the Model
+	char * model;
+
+	//The size of the T-Shirt can be xs,s,m,l,xl
+	char * size;
+
+	//The color of the T-shirt
+	char * color;
+
+	//The price of the T-shirt
+	double price;
+
+	//The number of T-shirts of this model the store keeps in storage
+	int count;
+
+};
